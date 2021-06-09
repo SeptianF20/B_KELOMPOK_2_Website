42e13b30acd11cffea910090db43878cd8b4f77e
SeptianF20 <71813937+SeptianF20@users.noreply.github.com>
2021-06-09 20:44:23 +0700

    uppdate

--- YOOVIS/resources/views/layouts/app.blade.php
+++ YOOVIS/resources/views/layouts/app.blade.php
@@ -1,14 +1,18 @@
+@extends('frontend/layouts.template')
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <div class="logo">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
