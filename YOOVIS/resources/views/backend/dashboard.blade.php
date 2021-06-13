797d819e0c0bcd2cadbfc1da3be668603b70cb74
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2021-06-14 00:50:03 +0700

    admin

--- YOOVIS/resources/views/backend/dashboard.blade.php
+++ YOOVIS/resources/views/backend/dashboard.blade.php
@@ -1,61 +1,109 @@
 @extends('backend/layouts.template')
+
 @section('content')
-<!--main content start-->
-<section id="main-content">
-    <section class="wrapper">
-        <!--overview start-->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
-                <ol class="breadcrumb">
-                    <li><i class="fa fa-home"></i><a href="{{url('home')}}">Home</a></li>
-                    <li><i class="fa fa-laptop"></i>Dashboard</li>
-                </ol>
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row align-items-center">
+        <div class="col-md-6 col-8 align-self-center">
+            <h3 class="page-title mb-0 p-0">Dashboard</h3>
+            <div class="d-flex align-items-center">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </nav>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                <div class="info-box blue-bg">
-                    <i class="fa fa-cloud-download"></i>
-                    <div class="count">6.674</div>
-                    <div class="title">Download</div>
+    </div>
+</div>
+<div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <!-- column -->
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Basic Table</h4>
+                                <h6 class="card-subtitle">Add class <code>.table</code></h6>
+                                <div class="table-responsive">
+                                    <table class="table user-table">
+                                        <thead>
+                                            <tr>
+                                                <th class="border-top-0">#</th>
+                                                <th class="border-top-0">First Name</th>
+                                                <th class="border-top-0">Last Name</th>
+                                                <th class="border-top-0">Username</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Deshmukh</td>
+                                                <td>Prohaska</td>
+                                                <td>@Genelia</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Deshmukh</td>
+                                                <td>Gaylord</td>
+                                                <td>@Ritesh</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Sanghani</td>
+                                                <td>Gusikowski</td>
+                                                <td>@Govinda</td>
+                                            </tr>
+                                            <tr>
+                                                <td>4</td>
+                                                <td>Roshan</td>
+                                                <td>Rogahn</td>
+                                                <td>@Hritik</td>
+                                            </tr>
+                                            <tr>
+                                                <td>5</td>
+                                                <td>Joshi</td>
+                                                <td>Hickle</td>
+                                                <td>@Maruti</td>
+                                            </tr>
+                                            <tr>
+                                                <td>6</td>
+                                                <td>Nigam</td>
+                                                <td>Eichmann</td>
+                                                <td>@Sonu</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!--/.info-box-->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
             </div>
-            <!--/.col-->
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
 
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                <div class="info-box brown-bg">
-                    <i class="fa fa-shopping-cart"></i>
-                    <div class="count">7.538</div>
-                    <div class="title">Purchased</div>
-                </div>
-                <!--/.info-box-->
-            </div>
-            <!--/.col-->
 
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                <div class="info-box dark-bg">
-                    <i class="fa fa-thumbs-o-up"></i>
-                    <div class="count">4.362</div>
-                    <div class="title">Order</div>
-                </div>
-                <!--/.info-box-->
-            </div>
-            <!--/.col-->
-
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                <div class="info-box green-bg">
-                    <i class="fa fa-cubes"></i>
-                    <div class="count">1.426</div>
-                    <div class="title">Stock</div>
-                </div>
-                <!--/.info-box-->
-            </div>
-            <!--/.col-->
-        </div>
-        <!--/.row-->
-    </section>
-</section>
 @endsection
--- YOOVIS/resources/views/backend/pendidikan/index.blade.php
+++ YOOVIS/resources/views/backend/pendidikan/index.blade.php
@@ -1,90 +1,122 @@
 @extends('backend/layouts.template')
 @section('content')
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper">
-        <!--overview start-->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3 class="page-header"><i class="icon_document_alt"></i> Riwayat Hidup</h3>
-                <ol class="breadcrumb">
-                    <li><i class="fa fa-home"></i><a href="{{ url('home') }}">Home</a></li>
-                    <li><i class="icon_document_alt"></i>Riwayat Hidup</li>
-                    <li><i class="fa fa-graduation-cap"></i>Pendidikan</li>
-                </ol>
-            </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-12">
-            <section class="panel">
-              <header class="panel-heading">
-                Pendidikan
-              </header>
-              <div class="panel-body">
-                @if ($message = Session::get('success'))
-                  <div class="alert alert-success">
-                    <p>{{ $message }}</p>
-                  </div>
-                @endif
-                <a href="{{ route('pendidikan.create') }}">
-                  <button class="btn btn-primary" type="button"><i class="fa fa-plus"> Tambah</i></button>
-                </a><br><br>
-                <table class="table table-striped table-advance table-hover">
-                  <tbody>
-                    <tr>
-                      <th><i class="icon_briefcase"></i> Pendidikan</th>
-                      <th><i class="icon_document"></i> Gelar</th>
-                      <th><i class="icon_calendar"></i> Tahun Masuk</th>
-                      <th><i class="icon_calendar"></i> Tahun Selesai</th>
-                      <th><i class="icon_cogs"></i> Action</th>
-                    </tr>
-                    @foreach ($pendidikan as $item)
-                      <tr>
-                        <td>{{$item->nama}}</td>
-                        <td>
-                          @if ($item->tingkatan == 1)
-                            TK
-                          @elseif ($item->tingkatan == 2)
-                            SD
-                          @elseif ($item->tingkatan == 3)
-                            SMP
-                          @elseif ($item->tingkatan == 4)
-                            SMA/SMK
-                          @elseif ($item->tingkatan == 5)
-                            D3
-                          @elseif ($item->tingkatan == 6)
-                            D4/S1
-                          @elseif ($item->tingkatan == 7)
-                            S2
-                          @elseif ($item->tingkatan == 8)
-                            S3
-                          @endif
-                        </td>
-                        <td>{{$item->tahun_masuk}}</td>
-                        <td>{{$item->tahun_lulus}}</td>
-                        <td>
-                          <div class="btn-group">
-                            <form action="{{ route('pendidikan.destroy',$item->id)}}" method="POST">
-                              <a href=" {{ route('pendidikan.edit',$item->id) }} " class="btn btn-warning"><i class="fa fa-edit"></i></a>
-                              @csrf
-                              @method('DELETE')
-                              <button type="submit" class="btn btn-danger" name="button"
-                              onclick="return confirm('Apakah anda yakin menghapus data ini ?')">
-                                <i class="fa fa-trash-o"></i>
-                              </button>
-                            </form>
-                          </div>
-                        </td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
+<div class="page-wrapper">
+  <!-- ============================================================== -->
+  <!-- Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <div class="page-breadcrumb">
+      <div class="row align-items-center">
+          <div class="col-md-6 col-8 align-self-center">
+              <h3 class="page-title mb-0 p-0">Pendidikan</h3>
+              <div class="d-flex align-items-center">
+                  <nav aria-label="breadcrumb">
+                      <ol class="breadcrumb">
+                          <li class="breadcrumb-item"><a href="#">Home</a></li>
+                          <li class="breadcrumb-item active" aria-current="page">Pendidikan</li>
+                      </ol>
+                  </nav>
               </div>
-            </section>
           </div>
-        </div>
-        <!--/.row-->
-    </section>
-</section>
+      </div>
+  </div>
+  <!-- ============================================================== -->
+  <!-- End Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+  <!-- Container fluid  -->
+  <!-- ============================================================== -->
+  <div class="container-fluid">
+      <!-- ============================================================== -->
+      <!-- Start Page Content -->
+      <!-- ============================================================== -->
+      <div class="row">
+          <!-- column -->
+          <div class="col-sm-12">
+              <div class="card">
+                  <div class="card-body">
+                      <h4 class="card-title">Tabel Pendidikan</h4>
+                      <div class="panel-body">
+                        @if ($message = Session::get('success'))
+                          <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                          </div>
+                        @endif
+                        <a href="{{ route('pendidikan.create') }}">
+                          <button class="btn btn-primary" type="button"><i class="fa fa-plus"> Tambah</i></button>
+                        </a><br><br>
+                      <div class="table-responsive">
+                        <table class="table table-striped table-advance table-hover">
+                          <tbody>
+                            <tr>
+                              <th><i class="icon_briefcase"></i> Pendidikan</th>
+                              <th><i class="icon_document"></i> Gelar</th>
+                              <th><i class="icon_calendar"></i> Tahun Masuk</th>
+                              <th><i class="icon_calendar"></i> Tahun Selesai</th>
+                              <th><i class="icon_cogs"></i> Action</th>
+                            </tr>
+                            @foreach ($pendidikan as $item)
+                              <tr>
+                                <td>{{$item->nama}}</td>
+                                <td>
+                                  @if ($item->tingkatan == 1)
+                                    TK
+                                  @elseif ($item->tingkatan == 2)
+                                    SD
+                                  @elseif ($item->tingkatan == 3)
+                                    SMP
+                                  @elseif ($item->tingkatan == 4)
+                                    SMA/SMK
+                                  @elseif ($item->tingkatan == 5)
+                                    D3
+                                  @elseif ($item->tingkatan == 6)
+                                    D4/S1
+                                  @elseif ($item->tingkatan == 7)
+                                    S2
+                                  @elseif ($item->tingkatan == 8)
+                                    S3
+                                  @endif
+                                </td>
+                                <td>{{$item->tahun_masuk}}</td>
+                                <td>{{$item->tahun_lulus}}</td>
+                                <td>
+                                  <div class="btn-group">
+                                    <form action="{{ route('pendidikan.destroy',$item->id)}}" method="POST">
+                                      <a href=" {{ route('pendidikan.edit',$item->id) }} " class="btn btn-warning"><i class="fa fa-edit"></i></a>
+                                      @csrf
+                                      @method('DELETE')
+                                      <button type="submit" class="btn btn-danger" name="button"
+                                      onclick="return confirm('Apakah anda yakin menghapus data ini ?')">
+                                        <i class="fa fa-trash-o"></i>
+                                      </button>
+                                    </form>
+                                  </div>
+                                </td>
+                              </tr>
+                            @endforeach
+                          </tbody>
+                        </table>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- ============================================================== -->
+      <!-- End PAge Content -->
+      <!-- ============================================================== -->
+      <!-- ============================================================== -->
+      <!-- Right sidebar -->
+      <!-- ============================================================== -->
+      <!-- .right-sidebar -->
+      <!-- ============================================================== -->
+      <!-- End Right sidebar -->
+      <!-- ============================================================== -->
+  </div>
+  <!-- ============================================================== -->
+  <!-- End Container fluid  -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Page wrapper  -->
 @endsection
