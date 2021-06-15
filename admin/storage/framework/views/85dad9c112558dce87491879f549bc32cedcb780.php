28ab027266dfc668a15e402943f88bb1e3ad6597
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2021-06-15 17:31:50 +0700

    form login+register

---
+++ admin/storage/framework/views/85dad9c112558dce87491879f549bc32cedcb780.php
@@ -0,0 +1,32 @@
+
+
+<?php $__env->startSection('content'); ?>
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row align-items-center">
+        <div class="col-md-6 col-8 align-self-center">
+            <h3 class="page-title mb-0 p-0">Dashboard</h3>
+            <div class="d-flex align-items-center">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('backend/layouts.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Semester 4\Coba_coba\admin\resources\views/backend/dashboard.blade.php ENDPATH**/ ?>
