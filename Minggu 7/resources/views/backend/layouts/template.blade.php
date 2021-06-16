67f7fd3448b68fff5a7402a7a79219bdcd501619
SeptianF20 <71813937+SeptianF20@users.noreply.github.com>
2021-06-16 08:24:31 +0700

    coba

--- Minggu 7/resources/views/backend/layouts/template.blade.php
+++
@@ -1,9 +0,0 @@
-@include('backend/layouts.header')
-<main class="content-wrapper">
-    <div class="mdc-layout-grid">
-        <div class="mdc-layout-grid__inner">
-            @yield('content')
-        </div>
-    </div>
-</main>
-@include('backend/layouts.footer')
