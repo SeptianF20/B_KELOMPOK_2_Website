4e46f222bc3f9d46b868386189c4e894b8393d99
ariniekanoviyanti <ariniekanoviyanti@gmail.com>
2021-06-22 19:18:49 +0700

    new

--- YOOVIS/resources/views/frontend/layouts/footer.blade.php
+++ YOOVIS/resources/views/frontend/layouts/footer.blade.php
@@ -4,7 +4,7 @@
       <div class="row d-flex align-items-center">
         <div class="col-lg-6 text-lg-left text-center">
           <div class="copyright">
-            &copy; Copyright <strong>Vesperr</strong>. All Rights Reserved
+            &copy; Copyright <strong>Kelompok 2</strong>. All Rights Reserved
           </div>
           <div class="credits">
             <!-- All the links in the footer should remain intact. -->
@@ -13,14 +13,6 @@
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
           </div>
-        </div>
-        <div class="col-lg-6">
-          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
-            <a href="#intro" class="scrollto">Home</a>
-            <a href="#about" class="scrollto">About</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Use</a>
-          </nav>
         </div>
       </div>
     </div>
